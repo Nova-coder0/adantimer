7f9a652ae282c726151b977d377588927708a12f
Nova-coder0 <creatonly0@gmail.com>
2026-04-11 17:18:25 +0200

    Add blob upload sentinel

---
+++ api/upload-sentinel-blob.js
@@ -0,0 +1,7 @@
+export const UPLOAD_SENTINEL_BLOB = {
+  arabicEscapes: "\\u0645\\u0648\\u0627\\u0642\\u064a\\u062a \\u0627\\u0644\\u0635\\u0644\\u0627\\u0629 \\u0641\\u064a \\u0628\\u0631\\u064a\\u062f\\u0629",
+  chineseEscapes: "\\u8fd9\\u4e2a\\u9875\\u9762\\u4f1a\\u663e\\u793a\\u54ea\\u4e9b\\u793c\\u62dc\\u65f6\\u95f4\\uff1f",
+  mixedTemplate: "Asr Time in ${place} Today | Adantimer",
+  routeIntent: "Built around the exact search intent for ${target}.",
+  footer: "Accurate prayer times for ${place} and other cities."
+};
